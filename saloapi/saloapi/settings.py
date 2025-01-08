e4d83aa0174f7867790972672a02e456634e61b1
Artem0904 <artemshadiuk@gmail.com>
2025-01-08 21:01:39 +0200

    create Cors

--- saloapi/saloapi/settings.py
+++ saloapi/saloapi/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
